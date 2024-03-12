88a51937a9c691972e40fa2048dbc168e8a4c3e9
DucKie0412 <zednqnb@gmail.com>
2024-03-12 16:18:04 +0700

    update project layout

---
+++ src/layouts/DefaultLayout/index.js
@@ -0,0 +1,18 @@
+import Header from "./Header/header";
+import Sidebar from "./Sidebar/sidebar";
+
+
+function DefaultLayout({ children }) {
+    return (
+        <div>
+            <Header />
+            <div className="container">
+                <Sidebar />
+                <div className="content">{children}</div>
+            </div>
+
+        </div>
+    );
+}
+
+export default DefaultLayout;
